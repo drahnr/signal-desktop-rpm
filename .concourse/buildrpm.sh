0282aa29e84713465880b2d56e57ad37d3ef4dff
Bernhard Schuster <bernhard@ahoi.io>
2018-03-04 09:27:46 +0100

    path madness within rpmbuild.sh

--- .concourse/buildrpm.sh
+++ .concourse/buildrpm.sh
@@ -3,33 +3,42 @@
 set -x
 set -e
 
-name="signal-desktop"
+REFERENCE_DIR="$(pwd)"
+
+NAME="${1}"
+SPEC_SOURCE_DIR="${REFERENCE_DIR}/${2}"
+GIT_REPO_DIR="${REFERENCE_DIR}/${3}"
+OUTPUT_RPM_DIR="${REFERENCE_DIR}/${4}"
+OUTPUT_SRPM_DIR="${REFERENCE_DIR}/${5}"
+
 pwd 2>&1
-RPMBUILD_DIR="$(pwd)/${1}/rpmbuild"
+RPMBUILD_DIR="${REFERENCE_DIR}/rpmbuild"
 
-mkdir -p ${RPMBUILD_DIR}/{SOURCES,BUILD,RPMS,SRPMS,SPECS}
+mkdir -p "${RPMBUILD_DIR}/{SOURCES,BUILD,RPMS,SRPMS,SPECS}"
 
-cp -v ${name}.spec ${RPMBUILD_DIR}/SPECS/
+cp -v "${SPEC_SOURCE_DIR}/${NAME}.spec" "${RPMBUILD_DIR}/SPECS"
 
-git archive --format=tar --prefix=${name}/ HEAD | xz > ${name}.tar.xz
+cd "${GIT_REPO_DIR}"
+git archive --format=tar --prefix="${NAME}" HEAD | xz > "${NAME}.tar.xz"
+cd -
 
-cp -v ${name}*.tar.xz ${RPMBUILD_DIR}/SOURCES/
+cp -v "${GIT_REPO_DIR}/${NAME}*.tar.xz" "${RPMBUILD_DIR}/SOURCES"
 
-cd ${RPMBUILD_DIR}
+cd "${RPMBUILD_DIR}"
 
 dnf install -y 'dnf-command(builddep)'
-dnf builddep -y SPECS/${name}.spec
+dnf builddep -y "SPECS/${NAME}.spec"
 
 rpmbuild \
---define "_topdir %(pwd)" \
+--define "_topdir ${REFERENCE_DIR}" \
 --define "_builddir %{_topdir}/BUILD" \
 --define "_rpmdir %{_topdir}/RPMS" \
 --define "_srcrpmdir %{_topdir}/SRPMS" \
 --define "_specdir %{_topdir}/SPECS" \
 --define "_sourcedir  %{_topdir}/SOURCES" \
--ba SPECS/${name}.spec || exit 1
+-ba "SPECS/${name}.spec" || exit 1
 
-mkdir -p $(pwd)/${1}/{,s}rpm/
-rm -vf ${RPMBUILD_DIR}/RPMS/x86_64/${name}-*debug*.rpm
-cp -vf ${RPMBUILD_DIR}/RPMS/x86_64/${name}-*.rpm $(pwd)/${1}/rpm/
-cp -vf ${RPMBUILD_DIR}/SRPMS/${name}-*.src.rpm $(pwd)/${1}/srpm/
+mkdir -p "${RPMBUILD_DIR}/{,s}rpm"
+rm -vf "${RPMBUILD_DIR}/RPMS/x86_64/${name}-*debug*.rpm"
+cp -vf "${RPMBUILD_DIR}/RPMS/x86_64/${name}-*.rpm" "${OUTPUT_RPM_DIR}"
+cp -vf "${RPMBUILD_DIR}/SRPMS/${name}-*.src.rpm" "${OUTPUT_SRPM_DIR}"
